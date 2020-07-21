ab28ac7156e461f3dc82b30a399eca346cc54ddf
Kevin Brown <kamauri205@gmail.com>
2020-07-21 12:30:41 +1000

    else statement if ans is wrong

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ document.getElementById("startreset").onclick = function () {
 
     show("timeremaining");
 
-    timeremaining = 5;
+    timeremaining = 60;
     document.getElementById("timeremainingvalue").innerHTML = timeremaining;
 
     // Hide game over box
@@ -40,10 +40,11 @@ document.getElementById("startreset").onclick = function () {
 
     generateQA();
   }
-}
+};
 //Clicking on an answer box
 document.getElementById("box1").onclick = function () {
-  if (playing == true) {//yes
+  if (playing == true) {
+    //yes
     if (this.innerHTML == correctAnswer) {
       //correct answer
 
@@ -53,63 +54,75 @@ document.getElementById("box1").onclick = function () {
       //hide wrong box and show correct box
       hide("wrong");
       show("correct");
+      setTimeout(function () {
+        hide("correct");
+      }, 1000);
+      
+       //Generate new Q&A
+       
+      generateQA();
+    } else {
+      hide("correct");
+      show("wrong");
+      setTimeout(function () {
+        hide("wrong");
+      }, 1000);
+    }
+  }
+};
 
-
+function startCountdown() {
+  action = setInterval(function () {
+    timeremaining -= 1;
+    document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+    if (timeremaining == 0) {
+      stopCountdown();
+      show("gameOver");
+      document.getElementById("gameOver").innerHTML =
+        "<p>Game over!</p><p>Your score is " + score + ".</p>";
+      hide("timeremaining");
+      hide("correct");
+      hide("wrong");
+      playing = false;
+      document.getElementById("startreset").innerHTML = "Start Game";
+    }
+  }, 1000);
 }
 
+function stopCountdown() {
+  clearInterval(action);
+}
 
-  function startCountdown() {
-    action = setInterval(function () {
-      timeremaining -= 1;
-      document.getElementById("timeremainingvalue").innerHTML = timeremaining;
-      if (timeremaining == 0) {
-        stopCountdown();
-        show("gameOver");
-        document.getElementById("gameOver").innerHTML =
-          "<p>Game over!</p><p>Your score is " + score + ".</p>";
-        hide("timeremaining");
-        hide("correct");
-        hide("wrong");
-        playing = false;
-        document.getElementById("startreset").innerHTML = "Start Game";
-      }
-    }, 1000);
-  }
-  function stopCountdown() {
-    clearInterval(action);
-  }
+function show(id) {
+  document.getElementById(id).style.display = "block";
+}
 
-  function show(id) {
-    document.getElementById(id).style.display = "block";
-  }
+function hide(id) {
+  document.getElementById(id).style.display = "none";
+}
 
-  function hide(id) {
-    document.getElementById(id).style.display = "none";
-  }
+function generateQA() {
+  var x = 1 + Math.round(9 * Math.random());
+  var y = 1 + Math.round(9 * Math.random());
+  correctAnswer = x * y;
+  document.getElementById("question").innerHTML = x + "x" + y;
+  var correctPosition = 1 + Math.round(3 * Math.random());
+  document.getElementById("box" + correctPosition).innerHTML = correctAnswer;
 
-  function generateQA() {
-    var x = 1 + Math.round(9 * Math.random());
-    var y = 1 + Math.round(9 * Math.random());
-    correctAnswer = x * y;
-    document.getElementById("question").innerHTML = x + "x" + y;
-    var correctPosition = 1 + Math.round(3 * Math.random());
-    document.getElementById("box" + correctPosition).innerHTML = correctAnswer;
+  //fill other boxes with wrong answers
 
-    //fill other boxes with wrong answers
+  var answers = [correctAnswer];
 
-    var answers = [correctAnswer];
-
-    for (i = 1; i < 5; i++) {
-      if (i != correctPosition) {
-        var wrongAnswer;
-        do {
-          wrongAnswer =
-            (1 + Math.round(9 * Math.random())) *
-            (1 + Math.round(9 * Math.random()));
-        } while (answers.indexOf(wrongAnswer) > -1);
-        document.getElementById("box" + i).innerHTML = wrongAnswer;
-        answers.push(wrongAnswer);
-      }
+  for (i = 1; i < 5; i++) {
+    if (i != correctPosition) {
+      var wrongAnswer;
+      do {
+        wrongAnswer =
+          (1 + Math.round(9 * Math.random())) *
+          (1 + Math.round(9 * Math.random()));
+      } while (answers.indexOf(wrongAnswer) > -1);
+      document.getElementById("box" + i).innerHTML = wrongAnswer;
+      answers.push(wrongAnswer);
     }
-  };
-
+  }
+};
